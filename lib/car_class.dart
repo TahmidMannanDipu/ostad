13c690dd81f2a26d4863473885993870a5855c7d
Tahmid Mannan Dipu <143392225+TahmidMannanDipu@users.noreply.github.com>
2024-09-27 23:27:13 +0600

    live test 01

---
+++ lib/car_class.dart
@@ -0,0 +1,13 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+
+  // Method to calculate the car's age
+  int carAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
-class Car {
-  String _brand;
-  String _model;
-  int _year;
 
-  Car(this._brand, this._model, this._year);
-
-  // Method to calculate the car's age
-  int carAge() {
-    int currentYear = DateTime.now().year;
-    return currentYear - _year;
-  }
-}
+import 'car_class.dart';
 
 void main() {
+  // Creating an instance of the Car class
   Car myCar = Car("Land Rover Defender", "130 8V 2024", 2021);
-  // Displaying the car's information directly using string interpolation
+
+  // Displaying the car's information
   print('Car Information:');
-  print('Brand: ${myCar._brand}');
-  print('Model: ${myCar._model}');
-  print('Year: ${myCar._year}');
+  print('Brand: ${myCar.brand}');
+  print('Model: ${myCar.model}');
+  print('Year: ${myCar.year}');
   print('Car Age: ${myCar.carAge()} years');
 }
